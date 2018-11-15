678a73cc48206c26e23712d7b951eeb63cda236b
wzheng <hb_wangzheng@163.com>
2018-11-15 17:33:09 +0800

    modified routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../lib/db')
+var Models = require('../lib/db');
+
+var userModel = Models.User;
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // userModel.findAll().then(function(users) {
+  //   console.log(users, '---------=');
+  // });
+  console.log();
+
+  res.render('index', { title: 'Express', users: [{ name: 'edsass' }] });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var Models = require('../lib/db');
+var UserModel = Models.User;
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
+router.post('/create', function(req, res, next) {
+  UserModel.create({
+    username: req.body.username
+  }).then(function(ee) {
+    res.redirect('/');
+  });
+});
 module.exports = router;
